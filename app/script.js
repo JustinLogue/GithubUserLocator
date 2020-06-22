1112bb1d1a519d3e6b9a4338927cdc010f5f8e6f
unknown <justin.noname@gmail.com>
2020-06-22 17:05:54 -0400

    4th commit

--- app/script.js
+++ app/script.js
@@ -6,7 +6,9 @@ app.innit = () =>{
 
 app.selectUser = () =>{
     $("#submitUser").on('click', function(){
-        $(".output").empty();
+        $(".output1").empty();
+        $(".output2").empty();
+        $(".output3").empty();
         const User= $('#user').val();
 
         console.log("You entered: ", User);
@@ -25,31 +27,48 @@ app.GetUser =(User) =>{
     const UserApi= 'https://api.github.com/users/'+User;
     const RepoApi = 'https://api.github.com/users/'+User+'/repos/';
     console.log("Searching for: ", result);
-    app.displayInfo(result);
+    if (result.message == "Not Found"|| User == "")
+    {
+        const htmlstring1=`
+        <h2>No Valid User Info Found</h2>`;
+        $(".output1").append(htmlstring1);
+    }
+    else{
+        app.displayInfo(result);
+    }
     
 })}
 
 app.displayInfo =(info) => {
     
     // console.log(info)
-    const htmlstring1 =`
-        <h2> Name: ${info.login}<\h2>
-        <img src="${info.avatar_url}" alt="the user's profile picture" width="200" height="200">
-        <br>
-        <a href=${info.html_url}>Go To Their Profile</a>
-        <p>Followers: ${info.followers}<br>
-        Following: ${info.following}<br>
-        Repos: ${info.public_repos}
-        </p>
-    `;
-    $(".output1").append(htmlstring1);
-    $.ajax({
-        url: 'https://api.github.com/users/'+info.login+'/repos',
-        method : "GET",
-        dataType: 'json',
-    }).then(result => {
-        app.displayRepos(result);
+    
+        const htmlstring1 =`
+            <h2> Name: ${info.login}<\h2>
+            <img src="${info.avatar_url}" alt="the user's profile picture" width="200" height="200">
+            <br>
+            <a href=${info.html_url}>Go To Their Profile</a>
+            <p>Followers: ${info.followers}<br>
+            Following: ${info.following}<br>
+            Repos: ${info.public_repos}
+            </p>
+        `;
+        $(".output1").append(htmlstring1);
+        $.ajax({
+            url: 'https://api.github.com/users/'+info.login+'/repos',
+            method : "GET",
+            dataType: 'json',
+        }).then(result => {
+            if (info.public_repos == 0){
+                const htmlstring4 = `
+                <h3> This user has no public repos... for now...</h3>`;
+                $('.output2').append(htmlstring4);
+            }
+            else{
+            app.displayRepos(result);
+            }
     })
+    
 }
 
 app.displayRepos =(info) =>{
@@ -58,6 +77,7 @@ app.displayRepos =(info) =>{
     var htmlstring3 = `<h3>
     Repo List</h3>`;
     $(".output2").append(htmlstring3);
+    if (info)
     info.forEach(repo => {
         if(repo.description ==null){
             htmlstring2=`
